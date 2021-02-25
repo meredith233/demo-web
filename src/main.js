17bedf1632271b7a8d000d50a19ebac374c1e848
梁炬鸿 <mrexgo@gmail.com>
2021-02-25 17:05:44 +0800

    引入三方资源

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+Vue.use(Vuex)
 
 new Vue({
   render: h => h(App),
